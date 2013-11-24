3251b54886b1b83cb29273f768b9d15379c9e798
Ravi Tejasvi <ravitejasvi@gmail.com>
2013-11-24 11:35:19 +0530

    gets type

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class HeadRequest(Request):
     def get_method(self):
         return "HEAD"
 
-url = 'http://stackoverflow.com/questions/22676/how-do-i-download-a-file-over-http-using-python'
+url = 'http://r13---sn-h557sne7.googlevideo.com/videoplayback?id=cffeeb39166d8b3f&itag=18&source=picasa&ip=27.251.237.154&ipbits=0&expire=1387864459&sparams=expire,id,ip,ipbits,itag,source&signature=2B7A765A0E78E5B36692BFC6C2326CB0FEC409C9.17C4161586B13F4D2CA75ED50DB28679662F3523&key=cms1&cms_redirect=yes&ms=nxu&mt=1385272523&mv=m'
 req = HeadRequest(url)
 response = urlopen(req)
 response.close()
@@ -21,7 +21,10 @@ size = content.get('Content-Length')
 step = int(size)//noOfThreads
 init= 0
 i=0
-f=open("try.html","ab+")
+fileType = content.get('Content-Type')
+fileType = fileType.split('/')
+fileName = "try"+'.'+fileType[1]
+f=open(fileName,"ab+")
 while(i!=noOfThreads):
 	req = Request(url, headers={'Range':'bytes=%s-%s'%(init,init+step)})
 	init=step+init
