8896f694d0f6e14c3c3a058693a7d394e59d2972
SamKT <samkt.93@gmail.com>
2013-11-28 21:21:47 +0530

    Update

--- PyGui.py
+++ PyGui.py
@@ -22,17 +22,24 @@ class Example(Frame):
         #print(url)
         download_object = download.Download_object(url, 4)
         init_download(download_object)
-        # Updating the treeview Entry. Retrieve The File Name from URL along with other information
-        # such as thread and size and add it to values field.
+        
         list_downloadObj.append(download_object)
-        counter_val = str(counter_entry)+'.'
-        counter_entry+=1
-        self.tree.insert('','end',values=(counter_val, download_object.fileName,
-                                          download_object.size,time.ctime(),download_object.noOfThreads))
         #send the url to respective function to download.
         downNow(download_object)
 
 
+    def update(self):
+        global counter_entry
+        url = self.area.get('1.0',END)
+        url = url.strip('\n')
+        #print(url)
+        download_object = download.Download_object(url, 4)
+        init_download(download_object)
+        counter_val = str(counter_entry)+'.'
+        counter_entry+=1
+        self.tree.insert('','end',values=(counter_val, download_object.fileName,
+                                          download_object.size,time.ctime(),'Downloaded',download_object.noOfThreads))
+        
     def centerWindow(self):
 
         width = 1000
@@ -47,10 +54,10 @@ class Example(Frame):
 
         #URL Field and label
         self.area = Text(self,height=2,width=50,bg='grey',fg='black')
-        self.area.place(x=250,y=150)
+        self.area.place(x=250,y=120)
         
         label_url = Label(self,text="Enter The URL:",bg='white',font=('Times New Roman',13))
-        label_url.place(x=100,y=153)
+        label_url.place(x=100,y=123)
 
         #List Entries
         listlabel = Label(self,text='Download List:',bg='white',font=('Times New Roman',13))
@@ -66,8 +73,10 @@ class Example(Frame):
         pause_button.place(x=440,y=60)
         remove_button = Button(self,text="REMOVE",width=12,font=('Times New Roman',11),command=remove)
         remove_button.place(x=600,y=60)
+        update_button = Button(self,text='UPDATE',width = 12,font=('Times New Roman',11),command=self.update)
+        update_button.place(x=300,y=190)
         download_button = Button(self,text="DOWNLOAD",width=14,font=('Times New Roman',11),command=self.fetch)
-        download_button.place(x=770,y=153)
+        download_button.place(x=570,y=190)
         #download_button.bind('<Button-1>',self.fetch(area))
         
         
@@ -81,8 +90,8 @@ class Example(Frame):
         self.tree.column('5',width=120)
         self.tree.heading('0',text="Sl.No")
         self.tree.heading('1',text="Name")
-        self.tree.heading('2',text='Size')
-        self.tree.heading('3',text='Modified')
+        self.tree.heading('2',text='Size (Bytes)')
+        self.tree.heading('3',text='Added On')
         self.tree.heading('4',text='Status')
         self.tree.heading('5',text='Threads')
         self.tree.place(x=120,y=280)
